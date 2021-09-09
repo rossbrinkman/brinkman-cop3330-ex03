b309a2fdd8793bc2254ded2eba1c767d2c0fca4d
rossbrinkman <55204801+rossbrinkman@users.noreply.github.com>
2021-09-09 17:39:23 -0400

    Finished 03

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Ross Brinkman
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "What is the quote?" );
+
+        Scanner scanner = new Scanner(System.in);
+        String quote = scanner.nextLine();
+        String author;
+
+        System.out.println( "Who said it?" );
+
+        author = scanner.nextLine();
+
+        System.out.println( author + " says, \"" + quote + "\"" );
+
+        scanner.close();
     }
 }
